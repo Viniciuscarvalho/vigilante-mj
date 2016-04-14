81f55f1844b057c4693a38b35a14a7de648546b5
viniciuscarvalho <viniciuscarvalho789@gmail.com>
2016-04-14 16:49:12 -0300

    Add InfoActivity

--- app/src/main/java/br/org/projeto/vigilante/ui/activities/InfoActivity.java
+++ app/src/main/java/br/org/projeto/vigilante/ui/activities/InfoActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.ViewById;
@@ -12,12 +11,12 @@ import org.androidannotations.annotations.ViewById;
 import br.org.projeto.vigilante.R;
 
 /**
- * Created by fernando on 4/13/16.
+ * Created by fernando on 4/14/16.
  */
-@EActivity(R.layout.activity_search_result)
-public class SearchResultActivity extends AppCompatActivity {
+@EActivity(R.layout.activity_info)
+public class InfoActivity extends AppCompatActivity {
 
-    @ViewById(R.id.search_result_toolbar)
+    @ViewById(R.id.info_toolbar)
     protected Toolbar mToolbar;
 
     @AfterViews
@@ -30,10 +29,5 @@ public class SearchResultActivity extends AppCompatActivity {
     @OptionsItem(android.R.id.home)
     protected void home() {
         supportFinishAfterTransition();
-    }
-
-    @Click(R.id.results)
-    protected void search() {
-        InfoActivity_.intent(this).start();
     }
 }
